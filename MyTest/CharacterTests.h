ae4f2caaba62a5821b3cd3a15424cbd8b9876d85
scorpiozj <scorpiozj@gmail.com>
2012-12-17 11:06:18 +0800

    Add
    add test cases

---
+++ MyTest/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Zhu J on 12/17/12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
---
+++ MyTest/WowUtilsTests.h
@@ -0,0 +1,12 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by Zhu J on 12/17/12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+@end
