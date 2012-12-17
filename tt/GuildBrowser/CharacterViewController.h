0cb6c809a700b837f541a9cdda8715d534d90433
scorpiozj <scorpiozj@gmail.com>
2012-12-17 10:34:26 +0800

    move to the father level

--- tt/GuildBrowser/CharacterViewController.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface CharacterViewController : UICollectionViewController
-    <UICollectionViewDataSource,
-    UICollectionViewDelegate,
-    UIPopoverControllerDelegate>
-@end
