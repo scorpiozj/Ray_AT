79c9117066f00c1b174e33ecffc01faeb323cf2e
scorpiozj <scorpiozj@gmail.com>
2012-12-17 10:46:33 +0800

    Add add New Test Target

---
+++ MyTest/MyTest.h
@@ -0,0 +1,13 @@
+//
+//  MyTest.h
+//  MyTest
+//
+//  Created by Zhu J on 12/17/12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MyTest : SenTestCase
+
+@end
---
+++ MyTest/MyTest.m
@@ -0,0 +1,32 @@
+//
+//  MyTest.m
+//  MyTest
+//
+//  Created by Zhu J on 12/17/12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import "MyTest.h"
+
+@implementation MyTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+   // STFail(@"Unit tests are not implemented yet in MyTest");
+}
+
+@end
